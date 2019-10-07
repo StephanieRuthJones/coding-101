85cef0f159c6c412efe6c3a9f651b3032c7cd348
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-10-07 13:37:11 -0600

    final commit

--- app.js
+++ app.js
@@ -10,50 +10,50 @@ const commentForm = document.querySelector("#comment-form")
 const commentInput = document.querySelector("#comment-input")
 const comments = document.querySelector("#comments-list")
 
-commentForm.addEventListener("submit", event => {
-    //without event.preventDefault
-    event.preventDefault()
-    //look at the event object to see target - will use different aspects of the target
-    console.log("event to look at event object and see the target", event)
+// commentForm.addEventListener("submit", event => {
+//     //without event.preventDefault
+//     event.preventDefault()
+//     //look at the event object to see target - will use different aspects of the target
+//     console.log("event to look at event object and see the target", event)
 
-    const comment = document.createElement("li")
-    comment.className = "comment"
-    comment.innerText = "Comment: " + commentInput.value
+//     const comment = document.createElement("li")
+//     comment.className = "comment"
+//     comment.innerText = "Comment: " + commentInput.value
 
-    // optional delete button if we have time:
-    const deleteButton = document.createElement("button")
-    deleteButton.innerText = "Delete"
+//     // optional delete button if we have time:
+//     const deleteButton = document.createElement("button")
+//     deleteButton.innerText = "Delete"
 
-    deleteButton.addEventListener('click', event => {
-        event.preventDefault()
-        console.log("delete button clicked", event.target.parentNode)
-        event.target.parentNode.remove()
+//     deleteButton.addEventListener('click', event => {
+//         event.preventDefault()
+//         console.log("delete button clicked", event.target.parentNode)
+//         event.target.parentNode.remove()
 
-    })
+//     })
 
-    comment.appendChild(deleteButton)
+//     comment.appendChild(deleteButton)
 
-    comments.appendChild(comment)
-    commentInput.value = ""
-})
+//     comments.appendChild(comment)
+//     commentInput.value = ""
+// })
 
 
 
-    //non-event delegation: add a click event listener to each list item
+//non-event delegation: add a click event listener to each list item
 
-    //const listItems = document.querySelectorAll('li')
-    // listItems.forEach(item => {
-    //     item.addEventListener('click', handleClick)
-    // })
-    // function handleClick(event) {
-    //     console.log(event.target.innerText)
-    // }
+//const listItems = document.querySelectorAll('li')
+// listItems.forEach(item => {
+//     item.addEventListener('click', handleClick)
+// })
+// function handleClick(event) {
+//     console.log(event.target.innerText)
+// }
 
-    //event delegation
-    // const instructions = document.querySelector('ol')
-    // instructions.addEventListener('click', seeInstruction)
+//event delegation
+// const instructions = document.querySelector('ol')
+// instructions.addEventListener('click', seeInstruction)
 
-    // function seeInstruction(event) {
-    //     console.log(event.target.innerText)
-    // }
+// function seeInstruction(event) {
+//     console.log(event.target.innerText)
+// }
 
