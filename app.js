2d87865740d7f685f8021c5e05d68d07c26b88ee
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2020-01-07 12:02:27 -0700

    Changed instructions in app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //1. Write an event listener for the comment form that adds a comment to the comment list
 
-//2. Add a delete button to each comment that removes comment when clicked
+//2. Use FormData() to add comment to comment list
 
-//3. Non-event delegation: Add a click event listener to each list item
+//3. Add a delete button to each comment that removes comment when clicked
 
-//4. Event delegation: Add a click event listener to list
+//4. Non-event delegation: Add a click event listener to each list item
+
+//5. Event delegation: Add a click event listener to list
 
